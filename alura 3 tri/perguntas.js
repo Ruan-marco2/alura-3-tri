056bb07c407c1d9aba373fd56c805565fc541406
Ruan-marco2 <ruan.milao@escola.pr.gov.br>
2024-09-17 08:53:11 -0300

    Add files via upload

---
+++ alura 3 tri/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um engenheiro da computação?',
+    'trabalham para garantir que os sistemas de computador de seus clientes sejam totalmente funcionais e eficientes.'
+)
+
+criaCartao(
+    'Ciência',
+    'O que é um acelerador de particulas?',
+    'São dispositivos que fornecem energia a feixes de partículas subatômicas eletricamente carregadas'
+)
+
+criaCartao(
+    'Hardware',
+    'O que significa a sigla SATA?',
+    'designa a tecnologia atual de transferência de dados entre os dispositivos de armazenamento e drives com o computador'
+)
+
+criaCartao(
+    'Jogo',
+    'O que é um RPG de mesa?',
+    'é um jogo que lembra muito os clássicos jogos de tabuleiro, porém com possibilidades mais amplas. Do inglês, a sigla RPG significa "Role Playing Game", um jogo de interpretar papéis.'
+)
